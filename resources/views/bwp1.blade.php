30aca8f9307050049f6a487f4b9b6ec80635cb28
severalpun <newrey9227@gmail.com>
2018-11-02 01:02:59 +0700

    update content jaringan prasarana bwp 1

--- resources/views/bwp1.blade.php
+++ resources/views/bwp1.blade.php
@@ -87,15 +87,16 @@
           </div>
         </div>
           <div class="awesome-project-content">
+
             <!-- single-awesome-project start -->
-            <div class="col-md-12 col-sm-12 col-xs-12 design development">
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
               <div class="single-awesome-project">
                 <div class="awesome-img">
-                  <a href="#"><img src="{{ url('content/muatan_rdtr')}}/pola_ruang.png" alt="" /></a>
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/5. PETA RENCANA PENGEMBANGAN JARINGAN JALAN_BWP 1.png" alt="" /></a>
                   <div class="add-actions text-center">
                     <div class="project-dec">
-                      <a class="venobox" data-gall="myGallery" href="{{ url('content/muatan_rdtr')}}/pola_ruang.png">
-                        <h4>Rencana Pola Ruang</h4>
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/5. PETA RENCANA PENGEMBANGAN JARINGAN JALAN_BWP 1.png">
+                        <h4>PENGEMBANGAN JARINGAN JALAN</h4>
                       </a>
                     </div>
                   </div>
@@ -103,6 +104,177 @@
               </div>
             </div>
             <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/6. PETA RENCANA PENGEMBANGAN PRASARANA JALAN_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/6. PETA RENCANA PENGEMBANGAN PRASARANA JALAN_BWP 1.png">
+                        <h4>RENCANA PENGEMBANGAN PRASARANA JALAN</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/7. PETA RENCANA PENGEMBANGAN RUTE ANGKUTAN UMUM_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/7. PETA RENCANA PENGEMBANGAN RUTE ANGKUTAN UMUM_BWP 1.png">
+                        <h4>RENCANA PENGEMBANGAN RUTE ANGKUTAN UMUM</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/8. PETA RENCANA JARINGAN PERKERETAAPIAN_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/8. PETA RENCANA JARINGAN PERKERETAAPIAN_BWP 1.png">
+                        <h4>RENCANA JARINGAN PERKERETAAPIAN</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/9. PETA RENCANA PENGEMBANGAN JARINGAN LISTRIK_BWP 1.mxd.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/9. PETA RENCANA PENGEMBANGAN JARINGAN LISTRIK_BWP 1.mxd.png">
+                        <h4>RENCANA PENGEMBANGAN JARINGAN LISTRIK</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/10. PETA RENCANA PENGEMBANGAN JARINGAN TELEKOMUNIKASI_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/10. PETA RENCANA PENGEMBANGAN JARINGAN TELEKOMUNIKASI_BWP 1.png">
+                        <h4>RENCANA PENGEMBANGAN JARINGAN TELEKOMUNIKASI</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/11. PETA RENCANA PENGEMBANGAN JARINGAN AIR MINUM_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/11. PETA RENCANA PENGEMBANGAN JARINGAN AIR MINUM_BWP 1.png">
+                        <h4>RENCANA PENGEMBANGAN JARINGAN AIR MINUM</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/12. PETA RENCANA PENGEMBANGAN JARINGAN DRAINASE_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/12. PETA RENCANA PENGEMBANGAN JARINGAN DRAINASE_BWP 1.png">
+                        <h4>RENCANA PENGEMBANGAN JARINGAN DRAINASE</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/13. PETA RENCANA PENGEMBANGAN JARINGAN AIR LIMBAH_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/13. PETA RENCANA PENGEMBANGAN JARINGAN AIR LIMBAH_BWP 1.png">
+                        <h4>RENCANA PENGEMBANGAN JARINGAN AIR LIMBAH</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/14. PETA RENCANA JALUR EVAKUASI BENCANA_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/14. PETA RENCANA JALUR EVAKUASI BENCANA_BWP 1.png">
+                        <h4>RENCANA JALUR EVAKUASI BENCANA</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
+            <!-- single-awesome-project start -->
+            <div class="col-md-3 col-sm-12 col-xs-12 design development">
+              <div class="single-awesome-project">
+                <div class="awesome-img">
+                  <a href="#"><img src="{{ url('content/bwp/BWP 1')}}/15. PETA RENCANA PENGEMBANGAN JARINGAN PERSAMPAHAN_BWP 1.png" alt="" /></a>
+                  <div class="add-actions text-center">
+                    <div class="project-dec">
+                      <a class="venobox" data-gall="myGallery" href="{{ url('content/bwp/BWP 1')}}/15. PETA RENCANA PENGEMBANGAN JARINGAN PERSAMPAHAN_BWP 1.png">
+                        <h4>RENCANA PENGEMBANGAN JARINGAN PERSAMPAHAN</h4>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- single-awesome-project end -->
+
           </div>
       </div>
     </div>
